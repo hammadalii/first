51ea7a4d59ed3feb0eaf4ce5ed7682cc21f6a0b8
hammadalii <hammadalia50@gmail.com>
2016-04-25 18:06:03 +0500

    my second committ

--- git/src/git/lab.java
+++ git/src/git/lab.java
@@ -5,6 +5,9 @@ public class lab {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello");
+System.out.println("Hello1");
+System.out.println("Hello2");
+System.out.println("Hello3");
 	}
 
 }
